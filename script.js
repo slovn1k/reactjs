b225690b76c959f4e3c35e5222949819cc5f5c4d
Turcanu Alexandr <turcanualexandru1992@gmail.com>
2019-12-11 21:57:15 +0200

    Arrows functions

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-let hello = "hello world";
-console.log(hello);
+// Declaring variables and constants
+let hello = "hello world"; // new method of declaring variables, old one is var
+const helloWorld = "Constant"; // here we declare a constant
+console.log(hello + " " + helloWorld);
+
+// Arrow functions
+// this is a simple function
+// function printMyName(name) {
+//     console.log(name);
+// }
+
+// this is an arrow function
+const printMyName = (name, age) => { // here we use paratheses because there are multiple parameters
+    console.log(name + " " + age);
+}
+
+printMyName("Alex", 27);
