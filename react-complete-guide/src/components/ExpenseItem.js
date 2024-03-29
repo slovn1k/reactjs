e9c5fbc012b48b7024894ba3518df0f6d4153887
Alexandr <turcanualexandru1992@gmail.com>
2024-03-29 11:07:40 +0200

    CSS component and styling html

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,17 @@
+// Here we import the css component
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+    return (
+        // Here we use className to add CSS classes to the html code
+        <div className="expense-item">
+            <div>Date</div>
+            <div className="expense-item__description">
+                <h2>Title</h2>
+                <div className="expense-item__price">Amount</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
