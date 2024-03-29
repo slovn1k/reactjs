71de407af59e735493f2991113832312303edd0e
Alexandr <turcanualexandru1992@gmail.com>
2024-03-29 11:36:37 +0200

    Send data via props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,27 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import logo from "./logo.svg";
+import "./App.css";
+// Here we import the component to use it in this route component
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ExpenseItem></ExpenseItem>
-      </header>
-    </div>
-  );
+    // here we create some objects with data
+    const expenses = [
+        {
+            id: "e1",
+            title: "Car Insurance",
+            amount: 345.67,
+            date: new Date(2024, 2, 30),
+        },
+        {
+            id: "e2",
+            title: "Car Insurance",
+            amount: 345.67,
+            date: new Date(2024, 2, 30),
+        },
+        {
+            id: "e3",
+            title: "Car Insurance",
+            amount: 345.67,
+            date: new Date(2024, 2, 30),
+        },
+        {
+            id: "e4",
+            title: "Car Insurance",
+            amount: 345.67,
+            date: new Date(2024, 2, 30),
+        },
+    ];
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                {/* Here we call the custom component to be rendered in th code */}
+                {/* Here we use props to send data */}
+                <ExpenseItem
+                    title={expenses[0].title}
+                    amount={expenses[0].amount}
+                    date={expenses[0].date}
+                ></ExpenseItem>
+                <ExpenseItem
+                    title={expenses[0].title}
+                    amount={expenses[0].amount}
+                    date={expenses[0].date}
+                ></ExpenseItem>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,17 +1,24 @@
 // Here we import the css component
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+// here we use props parameter to use parameters inside the function
+function ExpenseItem(props) {
+    const expenseDate = new Date(2024, 2, 29);
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 294.67;
+
     return (
         // Here we use className to add CSS classes to the html code
+        // Here we use the props parameter to get data sent
         <div className="expense-item">
-            <div>Date</div>
+            <div>{props.date.toLocaleString()}</div>
             <div className="expense-item__description">
-                <h2>Title</h2>
-                <div className="expense-item__price">Amount</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
 }
 
+// Here we export the component to use it
 export default ExpenseItem;
