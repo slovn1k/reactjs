2b0b0eb513c198b67346971b411e4483e2dc1e26
Alexandr <turcanualexandru1992@gmail.com>
2024-04-01 13:48:50 +0300

    Basic Components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 // Here we import the component to use it in this route component
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
     // here we create some objects with data
@@ -14,20 +14,20 @@ function App() {
         },
         {
             id: "e2",
-            title: "Car Insurance",
-            amount: 345.67,
+            title: "Car Insurance 2",
+            amount: 380.67,
             date: new Date(2024, 2, 30),
         },
         {
             id: "e3",
-            title: "Car Insurance",
-            amount: 345.67,
+            title: "Car Insurance 3",
+            amount: 350.67,
             date: new Date(2024, 2, 30),
         },
         {
             id: "e4",
-            title: "Car Insurance",
-            amount: 345.67,
+            title: "Car Insurance 4",
+            amount: 360.67,
             date: new Date(2024, 2, 30),
         },
     ];
@@ -47,18 +47,8 @@ function App() {
                 >
                     Learn React
                 </a>
-                {/* Here we call the custom component to be rendered in th code */}
-                {/* Here we use props to send data */}
-                <ExpenseItem
-                    title={expenses[0].title}
-                    amount={expenses[0].amount}
-                    date={expenses[0].date}
-                ></ExpenseItem>
-                <ExpenseItem
-                    title={expenses[0].title}
-                    amount={expenses[0].amount}
-                    date={expenses[0].date}
-                ></ExpenseItem>
+
+                <Expenses items={expenses} />
             </header>
         </div>
     );
--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,22 +1,14 @@
+import ExpenseDate from "./ExpenseDate";
 // Here we import the css component
 import "./ExpenseItem.css";
 
 // here we use props parameter to use parameters inside the function
 function ExpenseItem(props) {
-    // here we add some javascript to constants
-    const month = props.date.toLocaleString("en-US", { month: "long" });
-    const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-    const year = props.date.getFullYear();
-
     return (
         // Here we use className to add CSS classes to the html code
         // Here we use the props parameter to get data sent
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
